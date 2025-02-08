6b024f2f0c879593f0a6b59bcebc9ab2bdaedc76
Muhammad Ahsin Ismail <ahsan.ismail0159@gmail.com>
2025-02-08 21:46:04 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -161,7 +161,7 @@ function App() {
       <motion.div
         className="fixed inset-0 z-0"
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2)), url(${topLightImage})`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2)), url(${topPolygonImage})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           y: backgroundY,
